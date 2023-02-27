ae87771687bad405fbaa99fd88f020c7af664a87
FFellert <98618885+FFellert@users.noreply.github.com>
2023-02-27 14:47:38 +0100

    Wrong in the code

--- src/Entity/PlayerEntity.java
+++ src/Entity/PlayerEntity.java
@@ -1,24 +1,28 @@
 package Entity;
 
+import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class PlayerEntity extends Entity{
+
+    public LaserEntity laser = null;
 
     public PlayerEntity(Image image, double xPos, double yPos, int speed) {
         super(image, xPos, yPos, speed);
     }
 
-    public void left(int dx){
-        this.dx = dx;
+    public void left(){
+        dx = -1;
     }
-    public void right(int dx){
-        this.dx = dx;
+    public void right(){
+        dx = 1;
     }
-    public void down(int dy){
-        this.dy = dy;
+    public void down(){
+        dy = 1;
     }
-    public void up(int dy){
-        this.dy = dy;
+    public void up(){
+        dy = -1;
     }
 
     /**
@@ -31,7 +35,19 @@ public class PlayerEntity extends Entity{
         yPos += dy*(deltaTime/1000000000.0)*speed;
         xPos += dx*(deltaTime/1000000000.0)*speed;
     }
+    public boolean tryToFire() {
+        if(laser == null || !laser.getActive()){
+            laser = new LaserEntity(new ImageIcon(getClass().getResource("../resources/Laser.png")).getImage(), xPos+13, yPos, 90);
+            laser.setActive(true);
+            return true;
+        }else
+            return false;
+    }
+
     public void draw(Graphics2D g) {
+        if(laser != null && laser.getActive()){
+            laser.draw(g);
+        }
         super.draw(g);
     }
 }
