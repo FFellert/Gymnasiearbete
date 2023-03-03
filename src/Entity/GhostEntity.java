6ea84ad5b3ca96c37a76597aab2a72d7599f8023
FFellert <98618885+FFellert@users.noreply.github.com>
2023-03-03 11:30:41 +0100

    New getters

--- src/Entity/GhostEntity.java
+++ src/Entity/GhostEntity.java
@@ -3,19 +3,46 @@ package Entity;
 import java.awt.*;
 
 public class GhostEntity extends Entity {
-    public double Health;
-    public GhostEntity(Image image, double xPos, double yPos, int speed, double Health) {
+    public int Health;
+    public int dy;
+    public int dx;
+    public int Gold;
+    public GhostEntity(Image image, double xPos, double yPos, int speed, int Health, int dy, int dx, int Gold) {
         super(image, xPos, yPos, speed);
         this.Health = Health;
-        dy = 3;
+        this.Gold = Gold;
+        this.dy = dy;
+        this.dx = dx;
         this.setActive(false);
     }
-    public double getHealth() {
+    public int getHealth() {
         return Health;
     }
-    public void setHealth(double Health) {
+    public void setHealth(int Health) {
         this.Health = Health;
     }
+    public int getGold() {
+        return Gold;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+    public int getDx() {
+        return dx;
+    }
+    public int getDy() {
+        return dy;
+    }
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+
 
     /**
      * Metod som gör förflyttningen, dvs ändrar xPos och yPos
